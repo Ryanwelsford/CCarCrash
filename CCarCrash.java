185f5e87d85a8b29c719ecbec0c5e31a7652602b
Ryan <ryanwelsford@hotmail.com>
2019-01-22 20:53:00 +0000

    22nd additions, grid layout added in areas

--- CCarCrash.java
+++ CCarCrash.java
@@ -1,16 +1,16 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.Border;
  
  
 public class CCarCrash extends JFrame implements ActionListener
 {
 	// panels
 	private JPanel jPCentre, jPBottomRight, jPBottomLeft;
-	private JPanel jPRight, jPRightTop, jPRightBottom, jPRightMiddle;
-	private JPanel jPRightTop1, jPRTop2, jPRighTop3;
+	private JPanel jPRight, jPRightTop, jPRightBottom, jPRightMiddle, jPRightMovement;
 	
-	// text fields
+	// text fields in top right
     private JTextField jTFOption;
     private int nOption = 1;
     private JTextField jTFSquare;
@@ -18,16 +18,28 @@ public class CCarCrash extends JFrame implements ActionListener
     private JTextField jTFDirection;
     private String sDirection = "E";
     
-    // labels
-    private JLabel jLOption;
-    private JLabel jLSquare;
-    private JLabel jLDirection;
+    // text fields in right middle
+    private JTextField jTFTimer1, jTFTimer2, jTFTimer3;
+    
+    // labels in right top
+    private JLabel jLOption, jLSquare, jLDirection;
+
+    // labels in right middle
+    private JLabel jLTimer;
     
     // buttons options and exit (right hand side buttons)
     private JButton jBOption1, jBOption2, jBOption3;
     private JButton jBExit;
     // buttons for bottom area
     private JButton jBAct, jBRun, jBReset;
+    
+    // buttons for directional keys
+    // buttons for top row labelled top left top middle etc
+    private JButton jBTM, jBTL, jBTR;
+    // buttons for middle row labelled middle left etc
+    private JButton jBML, jBBlank, jBMR, jBMM;
+    // buttons for bottom row labelled bottom left etc
+    private JButton jBBM, jBBL, jBBR;
     
     // slider with slider label
     private JSlider jSSlider;
@@ -52,66 +64,133 @@ public class CCarCrash extends JFrame implements ActionListener
         
         //Centre
         jPCentre = new JPanel();
-        jPCentre.setPreferredSize(new Dimension(600, 500));
+        jPCentre.setPreferredSize(new Dimension(600, 560));
         jPCentre.setBackground(Color.orange);
         window.add(jPCentre);
         
         // right
         jPRight = new JPanel();
-        jPRight.setPreferredSize(new Dimension(180, 500));
-        jPRight.setBackground(Color.magenta);
+        jPRight.setPreferredSize(new Dimension(180, 560));
+        // jPRight.setBackground(Color.magenta);
         window.add(jPRight);
         
         // inside right
         
         jPRightTop = new JPanel();
         jPRightTop.setPreferredSize(new Dimension(180, 100));
-        jPRightTop.setBackground(Color.gray);
+        // jPRightTop.setBackground(Color.gray);
+        jPRightTop.setLayout(new GridLayout(3, 2));
         jPRight.add(jPRightTop);
         
-        // inside top right box needs panel 1 2 3 
+        jPRightMovement = new JPanel();
+        jPRightMovement.setPreferredSize(new Dimension(180, 100));
+        jPRightMovement.setBackground(Color.WHITE);
+        jPRightMovement.setLayout(new GridLayout(3, 3));
+        jPRight.add(jPRightMovement);
+        
+        
+        // inside right middle area
         
         jPRightMiddle = new JPanel();
-        jPRightMiddle.setPreferredSize(new Dimension(180, 300));
+        jPRightMiddle.setPreferredSize(new Dimension(180, 125));
         jPRightMiddle.setBackground(Color.green);
         jPRight.add(jPRightMiddle);
         
+        
+        // inside right bottom area
         jPRightBottom = new JPanel();
-        jPRightBottom.setPreferredSize(new Dimension(180, 100));
+        jPRightBottom.setPreferredSize(new Dimension(180, 75));
         jPRightBottom.setBackground(Color.gray);
+        jPRightBottom.setLayout(new GridLayout(2, 2));
         jPRight.add(jPRightBottom);
         
         // bottom
         jPBottomLeft = new JPanel();
-        jPBottomLeft.setPreferredSize(new Dimension (500,150));
+        jPBottomLeft.setPreferredSize(new Dimension (500, 40));
         jPBottomLeft.setBackground(Color.cyan);
         window.add(jPBottomLeft);
         
         jPBottomRight = new JPanel();
-        jPBottomRight.setPreferredSize(new Dimension(280, 150));
+        jPBottomRight.setPreferredSize(new Dimension(280, 40));
         jPBottomRight.setBackground(Color.BLUE);
         window.add(jPBottomRight);
         
         
         //git test fun times
-        
-        // options area in right top labels section
-        jLOption = new JLabel("Option");
+        // panel right top
+        // Panel right top labels and text fields
+        // Options label and text field
+        jLOption = new JLabel("Option: ");
         jPRightTop.add(jLOption); 
         jTFOption = new JTextField(""+nOption);
         jPRightTop.add(jTFOption);
         
         // square area label and text field
-        jLSquare = new JLabel("Square");
+        jLSquare = new JLabel("Square: ");
         jPRightTop.add(jLSquare);
         jTFSquare = new JTextField(""+nSquare);
         jPRightTop.add(jTFSquare);
         
         // Direction area label and text field
-        jLDirection = new JLabel("Direction");
+        jLDirection = new JLabel("Direction: ");
         jPRightTop.add(jLDirection);
         jTFDirection = new JTextField(sDirection);
         jPRightTop.add(jTFDirection);
+        
+        //panel right movement underneath right top
+        
+        // area for directional buttons 
+        // buttons for top row
+        jBBlank = new JButton();
+        jBBlank.setEnabled(false);
+        jPRightMovement.add(jBBlank);
+        
+        jBTM = new JButton("^");
+        jPRightMovement.add(jBTM);
+        
+        jBBlank = new JButton();
+        jBBlank.setEnabled(false);
+        jPRightMovement.add(jBBlank);
+
+        
+        //buttons for middle row
+        jBML = new JButton("<");
+        jPRightMovement.add(jBML);
+        
+        jBBlank = new JButton();
+        jBBlank.setEnabled(false);
+        jPRightMovement.add(jBBlank);
+        
+        jBMR = new JButton(">");
+        jPRightMovement.add(jBMR);
+        
+        //buttons for bottom row
+        jBBlank = new JButton();
+        jBBlank.setEnabled(false);
+        jPRightMovement.add(jBBlank);
+        
+        jBBM = new JButton("v");
+        jPRightMovement.add(jBBM);
+        
+        jBBlank = new JButton();
+        jBBlank.setEnabled(false);
+        jPRightMovement.add(jBBlank);
+        
+        //area for right middle 
+        // 3 text fields and a label required?
+        jLTimer = new JLabel("DIGITAL TIMER");
+        jPRightMiddle.add(jLTimer);
+        
+        jTFTimer1 = new JTextField("00");
+        jTFTimer1.setBackground(Color.BLACK);
+        jPRightMiddle.add(jTFTimer1);
+        
+        jTFTimer2 = new JTextField("00");
+        jPRightMiddle.add(jTFTimer2);
+        
+        jTFTimer3 = new JTextField("00");
+        jPRightMiddle.add(jTFTimer3);
+        
         
         // buttons exit option 1-3 placed within rightbottom
         jBOption1 = new JButton("Option 1");
@@ -165,7 +244,55 @@ public class CCarCrash extends JFrame implements ActionListener
     	nSquare = nSquare+1;
     	jTFSquare.setText(""+nSquare);
     	jTFDirection.setText("S");
+    	act();
+    	run();
+    	reset();
+    	option1();
+    	option2();
+    	option3();
+    	exit();
     	
+    }
+    // method act section
+    public void act()
+    {
+    	System.out.println("method act is working");
+    }
+    
+    //method run section
+    public void run()
+    {
+    	System.out.println("method run is working");
+    }
+    
+    //method reset
+    public void reset()
+    {
+    	System.out.println("method reset is working");
+    }
+    
+    //method option1
+    public void option1()
+    {
+    	System.out.println("method option1 is working");
+    }
+    
+    //method option2
+    public void option2()
+    {
+    	System.out.println("method option2 is working");
+    }
+    
+    //method option3
+    public void option3()
+    {
+    	System.out.println("method option1 is working");
+    }
+    
+    //method exit
+    public void exit()
+    {
+    	System.out.println("exit method is working");
     }
     
 }

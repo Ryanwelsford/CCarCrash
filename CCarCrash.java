c0235d490a14591badd68ccf5ed9e4aa7fb21a5c
Ryan <ryanwelsford@hotmail.com>
2019-02-12 17:48:42 +0000

    buttons now have different actions

--- CCarCrash.java
+++ CCarCrash.java
@@ -23,6 +23,8 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
     
     
     
+    
+    
     // labels in right top
     private JLabel jLOption, jLSquare, jLDirection;
     
@@ -44,13 +46,14 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
     
     
     // buttons for directional keys
-    // buttons for top row labelled top left top middle etc
+    // buttons labelled as bottom top middle
     private JButton jBTM;
-    // buttons for middle row labelled middle left etc
+    // buttons labelled as bottom middle left and right
     private JButton jBML, jBBlank, jBMR;
-    // buttons for bottom row labelled bottom left etc
+    // buttons for blank button is generic button used multiple times see blankbuttons()
     private JButton jBBM;
     // bottom bar 
+    
     // buttons for bottom area
     private JButton jBAct, jBRun, jBReset;
     
@@ -68,6 +71,8 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
         frame.setSize(810, 650);
         frame.createGUI();
         frame.setVisible(true);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
     }
     private void blankButtons()
     {
@@ -95,9 +100,9 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
     private void sliderTimer()
     {
     	int speedValue = jSSlider.getValue();
-        sliderTimer = new Timer(1000, this);
-        sliderTimer.start();
-    	sliderTimer.setDelay(speedValue);
+    	System.out.println(speedValue);
+    	secsTimer.setDelay(speedValue);
+    	// may need to change to its own timer at some point? slider timer does exist but breaks other timer atm
     }
  
     private void createGUI()
@@ -199,23 +204,27 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
         blankButtons();
 
         jBTM = new JButton("^");
+        jBTM.addActionListener(this);
         jPRightMovement.add(jBTM);
         
         blankButtons();
 
         //buttons for middle row
         jBML = new JButton("<");
+        jBML.addActionListener(this);
         jPRightMovement.add(jBML);
         
         blankButtons();
         
         jBMR = new JButton(">");
+        jBMR.addActionListener(this);
         jPRightMovement.add(jBMR);
         
         //buttons for bottom row
         blankButtons();
         
         jBBM = new JButton("v");
+        jBBM.addActionListener(this);
         jPRightMovement.add(jBBM);
         
         blankButtons();
@@ -244,8 +253,9 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
         jTFTimerSeconds.setForeground(Color.WHITE);
         jPRightMiddleTimer.add(jTFTimerSeconds);
         
-        secsTimer = new Timer(1000, this);
-        secsTimer.start();
+        
+        
+        
         
         
         
@@ -266,70 +276,119 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
         jPRightBottom.add(jBExit);
         jBExit.addActionListener(this);
         
+        //compass area 
+        
         // buttons act run reset
-        
-        jBAct = new JButton("Act");
-        // there must be a simplier way of adding an image to a button?
-        try {
-            Image img = ImageIO.read(getClass().getResource("resources/step.png"));
-            jBAct.setIcon(new ImageIcon(img));
-          } 
-        catch (Exception ex) {
-            System.out.println(ex);
-          }
-
-
+        ImageIcon actImage = new ImageIcon(getClass().getResource("resources/step.png"));
+        jBAct = new JButton("Act", actImage);
+        jBAct.addActionListener(this);
         jPBottomLeft.add(jBAct);
-        // this way is much simplier than try/ catch thing
-        ImageIcon runImg = new ImageIcon(getClass().getResource("resources/run.png"));
         
-        jBRun = new JButton("Run", runImg);
+        // this way is much simplier than try/ catch thing
+        ImageIcon runImage = new ImageIcon(getClass().getResource("resources/run.png"));
+        jBRun = new JButton("Run", runImage);
+        jBRun.addActionListener(this);
         jPBottomLeft.add(jBRun);
         
-        jBReset = new JButton("Reset");
+        ImageIcon resetImage = new ImageIcon(getClass().getResource("resources/reset.png"));
+        jBReset = new JButton("Reset", resetImage);
+        jBReset.addActionListener(this);
         jPBottomLeft.add(jBReset);
         
         // slider section
         jLSlider = new JLabel("Speed:");
         jPBottomRight.add(jLSlider);
         
-        jSSlider = new JSlider(JSlider.HORIZONTAL, 0, 100, 25);
-        jSSlider.setMajorTickSpacing(25);
+        jSSlider = new JSlider(JSlider.HORIZONTAL, 0, 2000, 1500);
+        jSSlider.setMajorTickSpacing(500);
         jSSlider.setPaintTicks(true);
         jSSlider.addChangeListener(this);
-       // jSSlider.setInverted(true); not sure if this works or not?
+        jSSlider.setInverted(true);
+        //sliderTimer = new Timer (1000, this);
+        //sliderTimer.start();
+        
+        
 
         // jSSlider.setBackground(Color.BLUE); you can set the colour of the background too!
         jPBottomRight.add(jSSlider);
         
+    
+        
         
     }
-   // public void actionPerformed(ActionEvent event)
-   // {
-   // 	System.exit(0);
-   // }
+
     public void actionPerformed(ActionEvent event)
     { 
+    	Object source = event.getSource();
+    	if (source == jBAct)
+    	{
+    		act();
+    	}
+    	
+    	else if (source == jBRun)
+    	{
+    		run();
+    	}
+    	
+    	else if (source == jBReset)
+    	{
+    		reset();
+    	}
+    	
+    	else if (source == jBOption1)
+    	{
+    		option1();
+    	}
+    	
+    	else if (source == jBOption2)
+    	{
+    		option2();
+    	}
+    	
+    	else if (source == jBOption3)
+    	{
+    		option3();
+    	}
+    	
+    	else if (source == jBExit)
+    	{
+    		exit();
+    	}
+    	
+    	else if (source == jBTM)
+    	{
+    		upButton();
+    	}
+    	
+    	else if (source == jBML)
+    	{
+    		leftButton();
+    	}
+    	
+    	else if (source == jBMR)
+    	{
+    		rightButton();
+    	}
+    	
+    	else if (source == jBBM)
+    	{
+    		downButton();
+    	}
     	// nOption = nOption+1;
     	/* jTFOption.setText(""+(nOption+1));
     	nOption = nOption+1;
     	nSquare = nSquare+1;
     	jTFSquare.setText(""+nSquare);
-    	jTFDirection.setText("S");
-    	act();
-    	run();
-    	reset();
-    	option1();
-    	option2();
-    	option3();
-    	exit(); */
-    	secsTimer();
+    	jTFDirection.setText("S"); */
     	
+    	
+    	secsTimer();
     	
     }
     public void stateChanged(ChangeEvent event)
     {
     	sliderTimer();
+    
     }
     // method act section
     public void act()
@@ -341,36 +400,87 @@ public class CCarCrash extends JFrame implements ActionListener, ChangeListener
     public void run()
     {
     	System.out.println("method run is working");
+    	
+    	secsTimer = new Timer(1000, this);
+        secsTimer.start();
     }
     
     //method reset
     public void reset()
     {
     	System.out.println("method reset is working");
+    	secsTimer.stop();
+    	secs = 00;
     }
     
     //method option1
     public void option1()
     {
     	System.out.println("method option1 is working");
+    	nOption = 1;
+    	jTFOption.setText(""+(nOption));
     }
     
     //method option2
     public void option2()
     {
     	System.out.println("method option2 is working");
+    	nOption = 2;
+    	jTFOption.setText(""+(nOption));
     }
     
     //method option3
     public void option3()
     {
-    	System.out.println("method option1 is working");
+    	System.out.println("method option3 is working");
+    	nOption = 3;
+    	jTFOption.setText(""+(nOption));
     }
     
     //method exit
     public void exit()
     {
     	System.out.println("exit method is working");
+    	System.exit(0);
     }
+    
+    // method up key pressed
+    public void upButton()
+    {
+    	System.out.println("up key pressed");
+    	sDirection = "N";
+    	jTFDirection.setText(sDirection);
+    	secs = secs-1;
+    }
+    
+    // method left key pressed
+    public void leftButton()
+    {
+    	System.out.println("left key pressed");
+    	sDirection = "W";
+    	jTFDirection.setText(sDirection);
+    }
+    
+    // method right key pressed
+    public void rightButton()
+    {
+    	System.out.println("right key pressed");
+    	sDirection = "E";
+    	jTFDirection.setText(sDirection);
+    }
+    
+    //method down key pressed
+    public void downButton()
+    {
+    	System.out.println("down key pressed");
+    	sDirection = "S";
+    	jTFDirection.setText(sDirection);
+    }
+    
+    
+    
+    
+    
+    // need more method for directional buttons
     
 }
